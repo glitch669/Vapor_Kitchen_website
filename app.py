83b45addc9e3bc602bf9eafe9ab62e8d4c8c5be3
dimple_k <68893917+DimpleK01@users.noreply.github.com>
2022-06-20 10:05:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,9 @@ import json
 import firebase_admin
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate({
-  "type": "service_account",
-  "project_id": "vapor-kitchen-aws6",
-  "private_key_id": "31f6b10295989cb0e9c72be15172df665339895b",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDmu0uwWbAOrfFM\nbf/6kdLsVwVgUdW5u0WP4jfHjjImxGu44vuzPJzjUwuC1H6CR/PjxxW+e1OsvNi8\n1ld/v7IKZtifYbqxp5165Lg1ht4JQR16s/l8cgEuRGIPdfcA2jMwvEuLhinI6yZf\nRkga4YEmIFfVs1yPPIaB8laeTQ9MN8KxLVHo0nD4tR2zTeBYAv+64ePISbkzGgYZ\n5mvl4CQ1Rdq3fnzJ9zASrFwhibtv/tW/AJ6/DSSGnZk6AgX410KqWO5L0qcX0ekA\n8pXsZMOGiUc5ibw97UHziGtYyv0Lub+vRJvYpnt1PWav5MfoKplcs3cp6CxZSjIY\nAEkczaorAgMBAAECggEAI45YBZ8XRcvW/tjctPe6h000Y47ONBxV20HDKBoTmcHJ\nz+i6sk5JkbEpzvQsDVZFO/lhXRnoNRsM/gdnMyU5Vz0DbjGYnD6g7V/zEIdDZC2y\npfFiJaPz5qCxySwmmkJbLSF3NX3BUVhbcMcwiWPti7s5jecMASQUnlCCufZUwIEe\n8KourY4sXAn0J0H/0VIxK8YcLC+MefKxZymBUopPGbFnKhIQ3lIWSgUJ7Nx7EE+O\nLPsuX1BI6vylLFFlktDbMaz7U8pmHVs4oHVWCC0sb4Gi0iP8Huycobm95LNdilH7\nGy+jW9TBcO8JMBNarrgSOdNeOHJOL94Z4cMTX5EMYQKBgQD/xIh0B5XXOw4YF40y\nMFEdj7j5SXypN1BeGwXEO7mU7LR49JBl5ffgXGE4Wa8vT7Mca0UaL6rZVegHUa49\nsLgfxqpSLCEuea6bfbpklHgMK3JrtG3Xv6rataiFCr5nKTc+zzdjltUaBH4E7GqG\nkrRNDBHlPhfeV3f9Yf+nAaIlqQKBgQDm8PEQLWUb8NS6chOKqvs+4FVPffI9a5go\nwFQ2rZcmDfeMFuLLSt6yuC+yj6v55csYRJLb2MaVDndpfrZScOM2ftNW3qxwDmWH\nlcIouHQyActPwW8ldoSfwnhWJZhmbI0ltdp1dgyYeIadn/FJv2GxACtYRGJHBgWI\nr0CiTz3NswKBgA8ja8wqUsUderUUV8gbsgFk61bd8aH2YyxevxWhT1ewNvJdz1oo\npWZDMYHrVQIg3oIVG070eVFJ02TzgbOnDQ7cbGJWuFQO4bXUWD7xxrU5sRkM4d9+\nObeePu4495IVj26JR0b+u3hBwc+6yodSkZdp2nlOFo2TcceukDorT5SZAoGAUS2u\nvy4F1Z2BWYyR+/OAX6jBtDCkfxycHu8Eazf34qOUOcUtsaI+x/ngolbIcA5rprss\nGGWJ4Il2RjoJYs9VvykzptydJsKYw+FLn76+8XkZ+44WQMf5dKMKY9XRTWeo8fGL\nujBl2w662xuG2JKSh4J0uhFHromaVmKsSKGsKusCgYEA8BsjFLegyjqLjGRCkaKG\n1yOjbDOG1+553sU0VlOaNWe4mh8QnBrII9Y/7VQZKevJ7/w1z4C6Fl5Kkmi7zv5f\nJHnZPr7DpaanB55X0+IjWmj/OT0OwlR9eIRnU5UjgJM6t6CoIYVKSbS7DgGGK19b\nilMfexsGPr+yzKKQpHxHLS8=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-9akyx@vapor-kitchen-aws6.iam.gserviceaccount.com",
-  "client_id": "111078690815716852922",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-9akyx%40vapor-kitchen-aws6.iam.gserviceaccount.com"
-})
+cred = credentials.Certificate(#add your credentials)
 
-firebase_admin.initialize_app(cred, {"databaseURL":"https://vapor-kitchen-aws6-default-rtdb.firebaseio.com/"})
+firebase_admin.initialize_app(cred, {#add your firebase URL})
 
 app = Flask(__name__)
 
@@ -73,9 +62,6 @@ def check():
         password = request.form['password']
         
         fdb = db.reference('userdata')
-
-        """if(email.lower() or email.upper() == 'd@gmail'):
-            return render_template("shopkeeper.html")"""
 
         if((email.lower() or email.upper()) in fdb.get()):
             fdb = db.reference('userdata/{}'.format(email))
