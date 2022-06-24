308befaa322de0eca7910faad083b50c24cf4b86
dimple_k <68893917+DimpleK01@users.noreply.github.com>
2022-06-24 13:15:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
-
-
 from flask import Flask, render_template, request, url_for
 import json
 import firebase_admin
 from firebase_admin import credentials, db
-
-cred = credentials.Certificate(#add your credentials)
-
-firebase_admin.initialize_app(cred, {#add your firebase URL})
-
+cred = credentials.Certificate(#ADD YOUR REALTIME DATABASE CREDENTIALS)
+firebase_admin.initialize_app(cred, {"databaseURL": #ADD YOUR DATABASE URL})
 app = Flask(__name__)
-
-
 @app.route('/')
 def index():
     return render_template('signup.html')
-
-
 @app.route('/signup', methods=['post'])
 def signup():
     if request.method == 'POST':
@@ -44,15 +35,12 @@ def signup():
     
         return render_template('login.html', msg=msg)
     return render_template('signup.html')
-
 @app.route('/login')
 def login():    
     return render_template('login.html')
-
 @app.route('/signup')
 def index2():    
     return render_template('signup.html')
-
 @app.route('/check',methods = ['post'])
 def check():
     if request.method=='POST':
@@ -62,7 +50,8 @@ def check():
         password = request.form['password']
         
         fdb = db.reference('userdata')
-
+        """if(email.lower() or email.upper() == 'd@gmail'):
+            return render_template("shopkeeper.html")"""
         if((email.lower() or email.upper()) in fdb.get()):
             fdb = db.reference('userdata/{}'.format(email))
             name = fdb.get()['name']
@@ -70,14 +59,12 @@ def check():
             
                 return render_template("home.html",name = name)
         return render_template("login.html")
-
 @app.route('/confirmcart',methods = ['post'])
 def confirmcart():
     if request.method=='POST':
         cart_json=request.get_json()
         cart = json.loads(cart_json) 
         cart = [json.loads(i) for i in cart]
-
         print("here", cart)
         items=[]
         prices=[]
@@ -96,7 +83,6 @@ def confirmcart():
         )
         return "Done"
         
-
 @app.route('/order',methods = ['post','get'])
 def order():
     if request.method=='POST' and request.form['Order Now']=='order':
@@ -117,17 +103,23 @@ def order():
         )
         return render_template("payment_page.html")
     
-
-
 @app.route('/get_orders', methods = ['post'])
 def get_orders():
     if request.method=='POST' and request.form['Confirm Payment']=='pay':
-        fdb= db.reference('userdata')
-        data = fdb.get()
-        return render_template("shopkeeper.html",data = data)
+        fdb= db.reference().child('userdata').get()
+        data = []
+        slno=0
+        for email in fdb.keys():
+            slno+=1
+            name = fdb[email]['name']
+            gender = fdb[email]['gender']
+            phone = fdb[email]['phone']
+            order_details= fdb[email]['order-details']
+            items = fdb[email]['cart-details']['items']
+            prices = fdb[email]['cart-details']['prices']
+            data.append([slno,name,gender,phone,order_details,items,prices])
+        return render_template("shopkeeper.html",data=data)
     return render_template("payment_page.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
