a7dbdc1c1fdd11711d77a51730a9f69a9c7e6977
dimple_k <68893917+DimpleK01@users.noreply.github.com>
2022-06-25 15:01:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 import json
 import firebase_admin
 from firebase_admin import credentials, db
 cred = credentials.Certificate(#ADD YOUR REALTIME DATABASE CREDENTIALS)
 firebase_admin.initialize_app(cred, {"databaseURL": #ADD YOUR DATABASE URL})
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     return render_template('signup.html')
+
+
 @app.route('/signup', methods=['post'])
 def signup():
     if request.method == 'POST':
@@ -22,9 +26,9 @@ def signup():
        
         fdb = db.reference('userdata')
         fdb.update({
-                email:{
+                srn:{
         'name': name,
-        'srn' : srn,
+        'email' : email,
         'gender': gender,
         'phone': phone,
         'password': password
@@ -34,45 +38,49 @@ def signup():
         msg= "Registration Complete. Please Login to your account !"
     
         return render_template('login.html', msg=msg)
-    return render_template('signup.html')
+    else:
+        msg= "Registration failed. Please signup again!"
+    return render_template('signup.html', msg=msg)
+
 @app.route('/login')
 def login():    
     return render_template('login.html')
+
 @app.route('/signup')
 def index2():    
     return render_template('signup.html')
+
 @app.route('/check',methods = ['post'])
 def check():
     if request.method=='POST':
         
-        global email 
-        email = request.form['email']
+        global srn 
+        srn = request.form['srn']
         password = request.form['password']
         
         fdb = db.reference('userdata')
-        """if(email.lower() or email.upper() == 'd@gmail'):
-            return render_template("shopkeeper.html")"""
-        if((email.lower() or email.upper()) in fdb.get()):
-            fdb = db.reference('userdata/{}'.format(email))
+
+        if((srn.lower() or srn.upper()) in fdb.get()):
+            fdb = db.reference('userdata/{}'.format(srn.lower()))
             name = fdb.get()['name']
             if password == fdb.get()["password"]:
             
                 return render_template("home.html",name = name)
         return render_template("login.html")
+
 @app.route('/confirmcart',methods = ['post'])
 def confirmcart():
     if request.method=='POST':
         cart_json=request.get_json()
         cart = json.loads(cart_json) 
         cart = [json.loads(i) for i in cart]
-        print("here", cart)
         items=[]
         prices=[]
         for i in range(len(cart)):
             items.append(cart[i]['productname'])
             prices.append(cart[i]['price'])
         
-        fdb= db.reference('userdata/{}'.format(email))
+        fdb= db.reference('userdata/{}'.format(srn.lower()))
         fdb.update({
             "cart-details":
             {
@@ -83,6 +91,7 @@ def confirmcart():
         )
         return "Done"
         
+
 @app.route('/order',methods = ['post','get'])
 def order():
     if request.method=='POST' and request.form['Order Now']=='order':
@@ -90,7 +99,7 @@ def order():
         blockname = request.form['blockname']
         roomnumber = request.form['roomnumber']
         
-        fdb= db.reference('userdata/{}'.format(email))
+        fdb= db.reference('userdata/{}'.format(srn.lower()))
         fdb.update({
             "order-details":
             {
@@ -103,23 +112,27 @@ def order():
         )
         return render_template("payment_page.html")
     
+
+
 @app.route('/get_orders', methods = ['post'])
 def get_orders():
     if request.method=='POST' and request.form['Confirm Payment']=='pay':
         fdb= db.reference().child('userdata').get()
         data = []
         slno=0
-        for email in fdb.keys():
+        for sdn in fdb.keys():
             slno+=1
-            name = fdb[email]['name']
-            gender = fdb[email]['gender']
-            phone = fdb[email]['phone']
-            order_details= fdb[email]['order-details']
-            items = fdb[email]['cart-details']['items']
-            prices = fdb[email]['cart-details']['prices']
+            name = fdb[sdn]['name']
+            print(name)
+            gender = fdb[sdn]['gender']
+            phone = fdb[sdn]['phone']
+            order_details= fdb[sdn]['order-details']
+            items = fdb[sdn]['cart-details']['items']
+            prices = fdb[sdn]['cart-details']['prices']
             data.append([slno,name,gender,phone,order_details,items,prices])
         return render_template("shopkeeper.html",data=data)
     return render_template("payment_page.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
